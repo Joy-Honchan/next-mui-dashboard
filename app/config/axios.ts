65e857e8c16455b274e886a17821adb7147ef1e5
Joy Fan <chandler0204@gmail.com>
2023-08-21 17:40:46 +0800

    chore: add api middleware

--- app/config/axios.ts
+++ app/config/axios.ts
@@ -1,6 +1,19 @@
 'use client'
 import axios from "axios"
+import { redirect } from 'next/navigation'
 
 if (localStorage.getItem('token')) {
     axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
 }
+
+axios.interceptors.response.use(
+    (res) => {
+        return res
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            redirect('/signout')
+        }
+        return Promise.reject(error)
+    }
+)
