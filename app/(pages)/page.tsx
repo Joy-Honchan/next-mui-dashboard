4f3eaf007e9ff950753833c43b61f45169373d2f
Joy Fan <chandler0204@gmail.com>
2023-08-25 15:20:26 +0800

    style: add image hover show btn

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -20,29 +20,69 @@ export default function Home() {
         <Paper
           sx={{
             height: '100%',
-            button: {
+            '.go-btn': {
               width: '100px',
               backgroundColor: 'pink',
               padding: '5px',
-              span: { position: 'relative', transition: 'all 0.5s' },
+              '.go-span': { position: 'relative', transition: 'all 0.5s' },
               '&:hover': {
                 span: {
                   paddingRight: '15px',
                   transition: 'all 0.5s'
                 },
-                'span::after': {
+                '.go-span::after': {
                   content: '"\u00BB"',
                   position: 'absolute',
                   right: 0,
                   transition: 'all 0.5s'
                 }
               }
+            },
+            '.img-container': {
+              maxWidth: '250px',
+              position: 'relative',
+              img: {
+                width: '100%'
+              },
+              '&:hover': {
+                '.overlay': {
+                  height: '100%',
+                  transition: 'all 0.5s'
+                }
+              }
+            },
+            '.overlay': {
+              position: 'absolute',
+              inset: 0,
+              top: 'auto',
+              backgroundColor: 'rgba(0,0,0,0.5)',
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 0,
+              overflow: 'hidden',
+              transition: 'all 3s',
+              '.click-btn': {
+                color: 'white',
+                backgroundColor: 'blue',
+                padding: '10px',
+                borderRadius: '10px'
+              }
             }
           }}
         >
-          <button>
-            <span>Go</span>
+          <button className="go-btn">
+            <span className="go-span">Go</span>
           </button>
+
+          <div className="img-container">
+            <img src="https://picsum.photos/id/1/200/300" alt="computer" />
+
+            <div className="overlay">
+              <button className="click-btn">Click Here</button>
+            </div>
+          </div>
         </Paper>
       </Grid>
       <Grid item xs={12} md={8} sx={{ height: '50%' }}>
